40d0bdeddf6175c3ebb3194a320e864d21010831
Dale Weber <dalew@hybotics.com>
2015-11-10 14:34:53 -0800

    Changed indentation to all tabs.

--- Go/src/bitbucket.org/hybotics.org/Artie/Artie.go
+++ Go/src/bitbucket.org/hybotics.org/Artie/Artie.go
@@ -1,30 +1,30 @@
 package main
 
 import (
-  "time"
-
-  "github.com/hybridgroup/gobot"
-  "github.com/hybridgroup/gobot/platforms/firmata"
-  "github.com/hybridgroup/gobot/platforms/gpio"
+	"time"
+	"github.com/hybridgroup/gobot"
+	"github.com/hybridgroup/gobot/platforms/firmata"
+	"github.com/hybridgroup/gobot/platforms/gpio"
+	"bitbucket.org/hybotics/appnet"
 )
 
 func main() {
-  artie := gobot.NewGobot()
+	artie := gobot.NewGobot()
 
-  firmataAdaptor := firmata.NewFirmataAdaptor("arduino", "/dev/tty.usbmodem411")
-  led := gpio.NewLedDriver(firmataAdaptor, "led", "13")
+	firmataAdaptor := firmata.NewFirmataAdaptor("arduino", "/dev/tty.usbmodem411")
+	led := gpio.NewLedDriver(firmataAdaptor, "led", "13")
 
-  work := func() {
-    gobot.Every(1*time.Second, func() {
-      led.Toggle()
-    })
-  }
+	work := func() {
+		gobot.Every(1*time.Second, func() {
+			led.Toggle()
+		})
+	}
 
-  artie.AddRobot(gobot.NewRobot("Artie",
-    []gobot.Connection{firmataAdaptor},
-    []gobot.Device{led},
-    work,
-  ))
+	artie.AddRobot(gobot.NewRobot("Artie",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{led},
+		work,
+	))
 
-  artie.Start()
+	artie.Start()
 }
